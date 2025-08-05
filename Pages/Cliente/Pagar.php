0d7a647d47762d42bdc3b5279231b4c75c013dfc
Alexia <custefanny@gmail.com>
2025-08-05 03:12:08 -0600

    Update Pagar.php

--- Pages/Cliente/Pagar.php
+++ Pages/Cliente/Pagar.php
@@ -4,13 +4,13 @@ include '../../conexion.php';
 
 // Verifica si la sesión está iniciada
 if (!isset($_SESSION['IdUsuario'])) {
-    header("Location: ../Pages/sitio/login.html");
+    header("Location: ../sitio/login.html");
     exit();
 }
 
 // Verifica si el rol es correcto
 if ($_SESSION['IdTipoUsuario'] != 3) {
-    header("Location: ../Pages/sitio/login.html");
+    header("Location: ../sitio/login.html");
     exit();
 }
 
@@ -781,5 +781,6 @@ if ($resOrden->num_rows > 0) {
     </script>
 
 </body>
+
 
 </html>
